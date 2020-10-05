19a4f6adae698435de2f2345a289c5c8ad58ed44
Djekill74 <jerome.delagneau@orange.fr>
2020-10-06 01:24:06 +0200

    debut

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log ('hello');
+console.log ('hello log');
+var year = 2018;
+
+if ((year % 4 === 0 && year % 100 > 0) || (year % 400 === 0)) { 
+	console.log (year + " est bissextile")
+} else {
+    console.log (year + " n'est pas bissextile")}
+    
+var number = 42;
+var reste = number % 10;
+var dizaine = (number - reste) / 10;
+
+console.log ("dans " + number + " il y a " + dizaine + " dizaine, et " + reste + " unité");
